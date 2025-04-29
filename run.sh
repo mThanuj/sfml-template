f3c2ad0fe428ccb540be311d3619029f135ee844
mThanuj <mullagurithanuj0@gmail.com>
2025-04-29 07:39:09 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -e
 
-cmake -S . -B build
+# Set default build type to Debug if no argument is passed
+BUILD_TYPE="${1:-debug}"
+
+# Check if the provided build type is valid
+if [[ "$BUILD_TYPE" != "debug" && "$BUILD_TYPE" != "release" ]]; then
+  echo "Invalid build type. Use 'debug' or 'release'."
+  exit 1
+fi
+
+# Configure CMake with the appropriate build type
+cmake -S . -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+
+# Build the project
 cmake --build build
 
+# Run the executable
 ./build/bin/CMakeSFMLProject
